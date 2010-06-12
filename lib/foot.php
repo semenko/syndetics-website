6a2fe02e096713740d0eab756c5f674fc3d03779
Nick Semenkovich <nick@calnexin.com>
2010-06-12 18:02:27 -0500

    Modularized.

---
+++ lib/foot.php
@@ -0,0 +1,20 @@
+		<!-- footer -->
+		<div class="footer">
+			<div class="left">
+				<p>Copyright &copy; <?=date('Y') ?> <a href="#" title="Syndetics, LLC">Syndetics, LLC</a>. All rights reserved.</p>
+			</div>
+			<div class="right">
+				<p> <a href="" title="right content">right content</a></p>
+			</div>
+		</div>
+		<!-- // footer -->
+			
+	</div>
+	<!-- // wrapper -->	
+	
+</div>
+<!-- // container -->
+
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
---
+++ lib/head.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+  <title>Syndetics, LLC</title>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta name="description" content="" />
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/screen.css" />
+   <? if (isset($custom_css)) { print '<link rel="stylesheet" type="text/css" media="screen" href="'.$custom_css.'" />'; } ?>
+  <!-- JavaScripts -->
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/jquery/jquery.cycle.js"></script>
+  <script type="text/javascript" src="assets/js/jquery/jquery.functions.js"></script>  
+  <!-- Cufon -->
+  <script type="text/javascript" src="assets/js/cufon/cufon.js"></script>
+  <script type="text/javascript" src="assets/js/cufon/aller.js"></script>
+  <script type="text/javascript" src="assets/js/cufon/config.js"></script>
+</head>
+<body>
+
+<!-- container -->
+<div class="container">
+	<!-- wrapper -->
+	<div class="wrapper">
+		<!-- header -->
+		<div class="header clearfix">	
+			<h1>Syndetics, LLC</h1>
+			<ul class="nav">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">About us</a></li>
+				<li><a href="services.html">Services</a></li>
+				<li><a href="portfolio.html">Portfolio</a></li>
+				<li><a href="contact.html">Contact us</a></li>				
+			</ul>			
+		</div>
+		<!-- // header -->
+
